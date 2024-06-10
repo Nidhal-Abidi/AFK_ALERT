cac8eeaed5fbeee4feee6ce27f660255e76fafcd
Nidhal-Abidi <nidhalabidi201@gmail.com>
2024-06-10 19:40:41 +0200

    wip: update the text shown in popup, build function that resets the timer once the user exits google meet tab
    Signed-off-by: Nidhal-Abidi <nidhalabidi201@gmail.com>

--- src/background.js
+++ src/background.js
@@ -1,7 +1,24 @@
-// `chrome.tabs.query` acts as a safety net, checking for existing Google Meet tabs when the extension is just installed or updated & there was a Google Meet tab opened before that.
-chrome.tabs.query({ currentWindow: true }, (tabs) => {
-  // Code this later.
-})
+function hasOpenGoogleMeetTab() {
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    const googleMeetPattern = /meet\.google\.com\/(.+)/
+    for (let tab of tabs) {
+      if (googleMeetPattern.test(tab.url)) {
+        return true
+      }
+    }
+    console.log("Google Meet tab is not open!!")
+    return false
+  })
+}
+
+async function hadOpenGoogleMeetTab() {
+  let res = await chrome.storage.local.get(["isGoogleMeetLink"])
+  if ("isGoogleMeetLink" in res && res["isGoogleMeetLink"] === true) {
+    console.log("Opened a google meet tab some time in the past!")
+    return true
+  }
+  return false
+}
 
 // `chrome.tabs.onUpdated.addListener` listens for future tab changes, catching newly opened Google Meet tabs.
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
@@ -9,7 +26,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status !== "complete") return
 
   if (tab.url && googleMeetPattern.test(tab.url)) {
-    chrome.storage.local.set({ isGoogleMeetLink: true })
+    chrome.storage.local.set({ isGoogleMeetLink: true, automaticTimerIn: 30 })
+    chrome.alarms.create("google-meet", {
+      periodInMinutes: 1 / 60,
+    })
   }
 })
 
@@ -19,14 +39,40 @@ chrome.alarms.create("start-countdown", {
 })
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "start-countdown") {
+  /* if (!hasOpenGoogleMeetTab() && hadOpenGoogleMeetTab()) {
+    // The user has an open Google meet tab then closed it. We should restart the timer
+    console.log("The timer is reset")
+    chrome.storage.local.set({
+      remainingTime: 3600,
+      isRunning: false,
+      isGoogleMeetLink: false,
+      automaticTimerIn: 30,
+    })
+    chrome.alarms.clear("google-meet")
+    chrome.alarms.clear("start-countdown")
+  } */
+
+  if (alarm.name === "google-meet") {
+    chrome.storage.local.get(["automaticTimerIn"], (res) => {
+      if (res["automaticTimerIn"] == 0) {
+        chrome.alarms.clear("google-meet")
+        chrome.storage.local.set({ isRunning: true })
+      } else {
+        chrome.storage.local.set({
+          automaticTimerIn: res["automaticTimerIn"] - 1,
+        })
+        chrome.action.setBadgeText({
+          text: (res["automaticTimerIn"] - 1).toString(),
+        })
+      }
+    })
+  } else if (alarm.name === "start-countdown") {
     chrome.storage.local.get(["remainingTime", "isRunning"], (res) => {
       if (res.isRunning) {
         let remainingTime = res.remainingTime - 1
         chrome.storage.local.set({ remainingTime })
         const remainingHours = Math.floor(remainingTime / 3600)
         const remainingMinutes = Math.floor((remainingTime / 60) % 60)
-        const remainingSeconds = Math.floor(remainingTime % 60)
         chrome.action.setBadgeText({
           text: `${remainingHours}:${remainingMinutes}`,
         })
@@ -36,10 +82,15 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 })
 
 // Initializing the vars in the local storage.
-chrome.storage.local.get(["remainingTime", "isRunning"], (res) => {
-  chrome.storage.local.set({
-    remainingTime: "remainingTime" in res ? res.remainingTime : 3600,
-    isRunning: "isRunning" in res ? res.isRunning : false,
-    isGoogleMeetLink: "isGoogleMeetLink" in res ? res.isGoogleMeetLink : false,
-  })
-})
+chrome.storage.local.get(
+  ["remainingTime", "isRunning", "isGoogleMeetLink", "automaticTimerIn"],
+  (res) => {
+    chrome.storage.local.set({
+      remainingTime: "remainingTime" in res ? res.remainingTime : 3600,
+      isRunning: "isRunning" in res ? res.isRunning : false,
+      isGoogleMeetLink:
+        "isGoogleMeetLink" in res ? res.isGoogleMeetLink : false,
+      automaticTimerIn: "automaticTimerIn" in res ? res.automaticTimerIn : 30,
+    })
+  }
+)
--- src/script.js
+++ src/script.js
@@ -23,17 +23,34 @@ resetBtn.addEventListener("click", () => {
   chrome.action.setBadgeText({ text: "" })
 })
 
-updateTimerContainer()
-setInterval(updateTimerContainer, 1000)
+updatePopup()
+setInterval(updatePopup, 1000)
+
+function updatePopup() {
+  updateTimerContainer()
+  updateTextDescription()
+}
 
 function updateTimerContainer() {
   chrome.storage.local.get(["remainingTime", "isGoogleMeetLink"], (res) => {
     let remainingTime = res["remainingTime"]
     const [hours, minutes, seconds] = updateTime(remainingTime)
     timerContainer.innerHTML = hours + "h " + minutes + "m " + seconds + "s"
+  })
+}
 
+function updateTextDescription() {
+  chrome.storage.local.get(["isGoogleMeetLink", "automaticTimerIn"], (res) => {
     if (res["isGoogleMeetLink"]) {
-      description.innerHTML = "I hope you have a productive meeting!!"
+      let text =
+        res["automaticTimerIn"] != undefined
+          ? `Timer starts automatically in ${res["automaticTimerIn"]}`
+          : ""
+
+      description.innerHTML = "I hope you have a productive meeting!!\n" + text
+    } else {
+      description.innerHTML =
+        "Please open a Google Meet page to automatically start the Timer!"
     }
   })
 }
